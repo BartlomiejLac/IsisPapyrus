502ebfcc3e94144a5b32199462522ea36c1b9d5f
BartlomiejLac <blaciak@student.agh.edu.pl>
2022-06-02 16:12:27 +0200

    zmiany ui

--- IsisPapyrus/Form1.cs
+++ IsisPapyrus/Form1.cs
@@ -28,14 +28,31 @@ namespace IsisPapyrus
             if (this.openFileDialog1.ShowDialog() == DialogResult.OK
                 && this.openFileDialog1.FileName.Length > 0)
             {
-                var input = CharStreams.fromPath(this.openFileDialog1.FileName);
-                this.syntaxRichTextBox1.Text = input.ToString();
-                IsisLexer lex = new IsisLexer(input);
-                CommonTokenStream tokens = new CommonTokenStream(lex);
-                IsisParser parser = new IsisParser(tokens);
-                ProgramContext start = parser.program();
+                this.syntaxRichTextBox1.Text = System.IO.File.ReadAllText(this.openFileDialog1.FileName);
+            }
+        }
 
-                this.textBox1.Text = start.ToStringTree(parser);
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var input = CharStreams.fromString(this.syntaxRichTextBox1.Text);
+            IsisLexer lex = new IsisLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lex);
+            IsisParser parser = new IsisParser(tokens);
+            ProgramContext start = parser.program();
+
+            this.textBox1.Text = start.ToStringTree(parser);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.saveFileDialog1.DefaultExt = "*.isis";
+            this.saveFileDialog1.Filter = "ISIS Files | *.isis";
+            this.saveFileDialog1.RestoreDirectory = true;
+
+            if (this.saveFileDialog1.ShowDialog() == DialogResult.OK
+                && this.saveFileDialog1.FileName.Length > 0)
+            {
+                System.IO.File.WriteAllText(this.saveFileDialog1.FileName, this.syntaxRichTextBox1.Text);
             }
         }
     }
